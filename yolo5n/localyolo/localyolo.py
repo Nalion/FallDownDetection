c29a6be287f0f28b5a804a302df64c18e98911ed
Nalion <1309008133@qq.com>
2024-10-21 22:18:57 +0800

    删除了不必要的文件

--- yolo5n/localyolo/localyolo.py
+++ yolo5n/localyolo/localyolo.py
@@ -1,6 +1,6 @@
 import torch
 
-model = torch.hub.load("ultralytics/yolov5", "custom", path="yolov5s.pt")  # PyTorch
+model = torch.hub.load("Nalion/FallDownDetection", "custom", path="yolov5n.pt")  # PyTorch
 
 # Images
 imgs = [f'./data/people({i+1}).jpg' for i in range(10)]  # batch of images
